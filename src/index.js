d25190269aa06bd3e9e614bcb15a3d050139acd0
DudaReis3 <133013268+DudaReis3@users.noreply.github.com>
2023-08-25 16:43:31 +0000

    Atividade 14

---
+++ src/index.js
@@ -0,0 +1,28 @@
+//index.js
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";   
+
+const app = express();              // Instancia o Express
+const port = 3000;                  // Define a porta
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+import roteadorUsuario from "./routes/usuario.js";
+import loginRouter from "./routes/login.js";
+
+app.use(roteadorUsuario);
+app.use(loginRouter);
+
+app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
+  res.json({
+    nome: "Maria Eduarda Reis Mendes",     
+  });
+  console.log("Rota / solicitada");
+});
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
