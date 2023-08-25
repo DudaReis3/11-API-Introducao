6f211b242427376c151fefab1e464c045f846263
DudaReis3 <133013268+DudaReis3@users.noreply.github.com>
2023-08-25 16:57:00 +0000

    Atividade

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 //index.js
 import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";   
+dotenv.config();
 
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
